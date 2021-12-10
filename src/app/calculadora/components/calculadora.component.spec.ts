7f2abbb8410ee42c9a1470c8c10d450905b83dcc
berkson <berksonx@yahoo.com.br>
2021-12-09 21:41:46 -0300

    Projeto cconcluido com o teste final do componente calculadora

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { CalculadoraService } from '../services';
 
 import { CalculadoraComponent } from './calculadora.component';
@@ -29,5 +30,53 @@ describe('CalculadoraComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  });
+
+  it('deve garantir que .3 + 2 = 2.3', () => {
+    let btnPonto = fixture.debugElement.query(By.css('#btnDecimal'));
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btnPonto.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('2.3');
   });
 });
